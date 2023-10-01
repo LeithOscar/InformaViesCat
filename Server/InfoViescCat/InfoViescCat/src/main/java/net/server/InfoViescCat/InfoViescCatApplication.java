bc82c4f203f350d5c8bf9baea451fb9bc7fcb078
LeithOscar <leithoscar@hotmail.com>
2023-10-01 18:54:22 +0200

    Create project. Api Login Check.

---
+++ Server/InfoViescCat/InfoViescCat/src/main/java/net/server/InfoViescCat/InfoViescCatApplication.java
@@ -0,0 +1,13 @@
+package net.server.InfoViescCat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoViescCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoViescCatApplication.class, args);
+	}
+
+}
---
+++ Server/InfoViescCat/InfoViescCat/src/test/java/net/server/InfoViescCat/InfoViescCatApplicationTests.java
@@ -0,0 +1,13 @@
+package net.server.InfoViescCat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoViescCatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
